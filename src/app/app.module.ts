dc52ceea6f94f0e2bb3a79643b8de070fdc35cc3
guilluch <guillaume.lluch@etu.univ-amu.fr>
2018-09-12 12:55:57 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+
+const appRoutes: Routes = [
+  // { path: '', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true}
+    ),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
