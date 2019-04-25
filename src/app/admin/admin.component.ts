7ebb86d57839ad54d4956fc3de2ce9f3af487443
uapv-lluch <guillaume.lluch@alumni.univ-avignon.fr>
2019-04-25 17:37:26 +0200

    Commit

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {QueriesService} from '../services/queries.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,8 +9,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  factorioSaves = [];
+  data = {
+    creationSelect: 'default'
+  };
+
+  constructor(private queriesService: QueriesService) { }
 
   ngOnInit() {
+    this.getFactorioSaves();
+  }
+
+
+  // TEAMSPEAK
+
+  startTS(event) {
+    this.queriesService.startTS().subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  stopTS(event) {
+    this.queriesService.stopTS().subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  restartTS(event) {
+    this.queriesService.restartTS().subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+
+  // FACTORIO
+
+  getFactorioSaves() {
+    this.queriesService.getFactorioSaves().subscribe((value) => {
+      console.log(value);
+      this.factorioSaves = value;
+    });
+  }
+
+  createFactorio(name: string) {
+    this.factorioSaves.push(name);
+  }
+
+  deleteFactorio(name: string) {
+  }
+
+  startFactorio(name: NgForm, event) {
+    // event.preventDefault();
+    console.log(name.value);
+    /*this.queriesService.startFactorio(name).subscribe((value) => {
+      console.log(value);
+    });*/
+  }
+
+  stopFactorio(event) {
+    /*this.queriesService.stopFactorio().subscribe((value) => {
+      console.log(value);
+    });*/
+  }
+
+  restartFactorio(name: string, event) {
+    this.queriesService.restartFactorio(name).subscribe((value) => {
+      console.log(value);
+    });
   }
 }
--- src/app/server-selector/server-selector.component.ts
+++ src/app/server-selector/server-selector.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
   selector: 'app-server-selector',
   templateUrl: './server-selector.component.html',
